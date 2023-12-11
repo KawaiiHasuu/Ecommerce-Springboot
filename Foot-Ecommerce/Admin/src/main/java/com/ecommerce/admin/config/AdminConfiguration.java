a37cdc825f0ff0073da1f0bd306aa078b55efb04
Nguyen Minh An <annm01032002@gmail.com>
2023-12-12 01:30:53 +0700

    Delete temporary file

--- Foot-Ecommerce/Admin/src/main/java/com/ecommerce/admin/config/AdminConfiguration.java
+++ Foot-Ecommerce/Admin/src/main/java/com/ecommerce/admin/config/AdminConfiguration.java
@@ -37,6 +37,7 @@ public class AdminConfiguration extends WebSecurityConfigurerAdapter{
         auth.authenticationProvider(daoAuthenticationProvider());
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/*").permitAll()
